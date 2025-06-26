d29e94839775432a9ba7dd7c8983a3808cf4a027
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-26 16:13:29 +0530

    Fixed build errors: RFPDetailCreation.tsx

--- src/pages/RFPDetailCreation.tsx
+++ src/pages/RFPDetailCreation.tsx
@@ -161,13 +161,15 @@ const RFPDetailCreationPage = () => {
       <CollapsibleSidebar />
       <div className="flex flex-col flex-1">
         <Header />
-        <main className="flex-1 p-4 md:p-6">
+        <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+          <div className="py-8">
             <MultiStepWizard
               steps={steps}
               onFinish={onWizardFinish}
               wizardTitle="Create New RFP"
               wizardDescription="Fill out the details below to add a new Request for Proposal to the system."
             />
+          </div>
         </main>
         <Footer />
       </div>
